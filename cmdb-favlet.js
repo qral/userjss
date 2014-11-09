177151ba9d5cba12fcbce2bef94e9c295215fc12
Adam Dvořák <adam.dvorak@gmail.com>
2014-11-09 20:04:40 +0100

    Create cmdb-favlet.js

---
+++ cmdb-favlet.js
@@ -0,0 +1,18 @@
+/* favlet pro CMDB */
+var h=prompt("hostname?");
+window.location.href = 'https://cmdbitaly.mil.esni.ibm.com/cmdb/script/query_sys0.php?CSH=1&VSH='+ h +'&CGEC=0&VAI=Submit';
+
+javascript:var h=prompt("hostname?");window.location.href = 'https://cmdbitaly.mil.esni.ibm.com/cmdb/script/query_sys0.php?CSH=1&VSH='+ h +'&CGEC=0&VAI=Submit';
+
+
+
+var ip=prompt("ip?");
+window.location.href = 'https://cmdbitaly.mil.esni.ibm.com/cmdb/script/query_sys0.php?CIP=1&VIP='+ip+'&CGEC=0&VAI=Submit';
+
+javascript:var ip=prompt("ip?");window.location.href = 'https://cmdbitaly.mil.esni.ibm.com/cmdb/script/query_sys0.php?CIP=1&VIP='+ip+'&CGEC=0&VAI=Submit';
+
+
+
+teoreticky by slo zobrazit i vsechny IP
+https://cmdbitaly.mil.esni.ibm.com/cmdb/sw/ml_ip_nodo.php?ID=5032735&SH=IT012DCSV1GSNFM&PT=WIN
+https://cmdbitaly.mil.esni.ibm.com/cmdb/sw/ml_ip_nodo.php?ID=5035118&SH=MILSNIIR1WREP01&PT=WIN
