1ef15c55654cb65392f583ebee45cd3f770ac250
A.D. <adam.dvorak@gmail.com>
2015-03-18 20:23:28 +0100

    Update netcool_clip.meta.js
    test

--- netcool_clip.meta.js
+++ netcool_clip.meta.js
@@ -1,4 +1,14 @@
 // ==UserScript==
 // @name        netcool clip
-// @version     1.2
+// @namespace   adam.dvorak+ibm@gmail.com
+// asdasd@asddownloadURL 
+// @updateURL   https://raw.githubusercontent.com/qral/userjss/master/netcool_clip.meta.js
+// @version     1.4
+// @description		ALL UR CLIPBOARD ARE BELONG TO US  -  copy2clipboard hack - thank you, you are making our job easier
+// @include https://195.183.244.178:16311/ibm/console/contentRender.do*
+// @include https://195.183.244.187:16311/ibm/console/contentRender.do*
+// @include https://195.183.244.178:16311/ibm/console/webtop/SATAweb/sendEvent*
+// @include https://195.183.244.187:16311/ibm/console/webtop/SATAweb/sendEvent*
+// @run-at document-start
+// @license BEERWARE
 // ==/UserScript==
